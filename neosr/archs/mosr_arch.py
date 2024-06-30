3b880c70ec4b5a6db570e4465bb15c00900401b4
umzi <ab5447612@gmail.com>
2024-06-30 16:53:36 +0300

    fix amp

--- neosr/archs/mosr_arch.py
+++ neosr/archs/mosr_arch.py
@@ -8,97 +8,6 @@ from neosr.utils.registry import ARCH_REGISTRY
 upscale, __ = net_opt()
 
 
-class attention2d(nn.Module):
-    def __init__(self, in_planes, ratios, K, temperature, init_weight=True):
-        super(attention2d, self).__init__()
-        assert temperature % 3 == 1
-        self.avgpool = nn.AdaptiveAvgPool2d(1)
-        if in_planes != 3:
-            hidden_planes = int(in_planes * ratios) + 1
-        else:
-            hidden_planes = K
-        self.fc1 = nn.Conv2d(in_planes, hidden_planes, 1, bias=False)
-        self.fc2 = nn.Conv2d(hidden_planes, K, 1, bias=True)
-        self.temperature = temperature
-        if init_weight:
-            self._initialize_weights()
-
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-            if isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-    def updata_temperature(self):
-        if self.temperature != 1:
-            self.temperature -= 3
-            print('Change temperature to:', str(self.temperature))
-
-    def forward(self, x):
-        x = self.avgpool(x)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x).view(x.size(0), -1)
-        return F.softmax(x / self.temperature, 1)
-
-
-class Dynamic_conv2d(nn.Module):
-    def __init__(self, in_planes, out_planes, kernel_size, ratio=0.25, stride=1, padding=0, dilation=1, groups=1,
-                 bias=True, K=4, temperature=34, init_weight=True):
-        super(Dynamic_conv2d, self).__init__()
-        assert in_planes % groups == 0
-        self.in_planes = in_planes
-        self.out_planes = out_planes
-        self.kernel_size = kernel_size
-        self.stride = stride
-        self.padding = padding
-        self.dilation = dilation
-        self.groups = groups
-        self.bias = bias
-        self.K = K
-        self.attention = attention2d(in_planes, ratio, K, temperature)
-
-        self.weight = nn.Parameter(torch.randn(K, out_planes, in_planes // groups, kernel_size, kernel_size),
-                                   requires_grad=True)
-        if bias:
-            self.bias = nn.Parameter(torch.zeros(K, out_planes))
-        else:
-            self.bias = None
-        if init_weight:
-            self._initialize_weights()
-
-    def _initialize_weights(self):
-        for i in range(self.K):
-            nn.init.kaiming_uniform_(self.weight[i])
-
-    def update_temperature(self):
-        self.attention.updata_temperature()
-
-    def forward(self, x):
-        softmax_attention = self.attention(x)
-        batch_size, in_planes, height, width = x.size()
-        x = x.reshape(1, -1, height, width)
-        weight = self.weight.view(self.K, -1)
-
-        aggregate_weight = torch.mm(softmax_attention, weight).view(batch_size * self.out_planes,
-                                                                    self.in_planes // self.groups, self.kernel_size,
-                                                                    self.kernel_size)
-        if self.bias is not None:
-            aggregate_bias = torch.mm(softmax_attention, self.bias).view(-1)
-            output = F.conv2d(x, weight=aggregate_weight, bias=aggregate_bias, stride=self.stride, padding=self.padding,
-                              dilation=self.dilation, groups=self.groups * batch_size)
-        else:
-            output = F.conv2d(x, weight=aggregate_weight, bias=None, stride=self.stride, padding=self.padding,
-                              dilation=self.dilation, groups=self.groups * batch_size)
-
-        output = output.view(batch_size, self.out_planes, output.size(-2), output.size(-1))
-        return output
-
-
 class GatedCNNBlock(nn.Module):
     r""" Our implementation of Gated CNN Block: https://arxiv.org/pdf/1612.08083
     Args:
@@ -146,10 +55,11 @@ class GatedBlocks(nn.Module):
                  expansion_ratio
                  ):
         super().__init__()
-        self.in_to_out = Dynamic_conv2d(in_dim, out_dim, 3, padding=1)
-
+        self.in_to_out = nn.Conv2d(in_dim, out_dim, kernel_size=3, padding=1)
         self.gcnn = nn.Sequential(
-            *[GatedCNNBlock(out_dim, expansion_ratio=expansion_ratio, drop_path=drop_path)
+            *[GatedCNNBlock(out_dim,
+                            expansion_ratio=expansion_ratio,
+                            drop_path=drop_path)
               for _ in range(n_blocks)
               ])
 
@@ -165,7 +75,7 @@ class mosr(nn.Module):
     def __init__(self,
                  in_ch: int = 3,
                  out_ch: int = 3,
-                 upscale: int = 4,
+                 upscale: int = upscale,
                  blocks: tuple[int] = (3, 3, 9, 3),
                  dims: tuple[int] = (48, 96, 192, 288),
                  upsampler: str = "ps",
@@ -176,7 +86,9 @@ class mosr(nn.Module):
         len_blocks = len(blocks)
         dims = [in_ch] + list(dims)
         self.gblocks = nn.Sequential(
-            *[GatedBlocks(dims[i], dims[i + 1], blocks[i], drop_path=drop_path, expansion_ratio=expansion_ratio)
+            *[GatedBlocks(dims[i], dims[i + 1], blocks[i], drop_path=drop_path,
+                          expansion_ratio=expansion_ratio
+                          )
               for i in range(len_blocks)]
         )
 
